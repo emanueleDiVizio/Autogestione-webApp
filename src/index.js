83d0a731e7498efda1bd544d5b7bf174750e30ca
emanuele_di <emadivizio@gmail.com>
2016-12-11 17:34:43 +0100

    login and signup

--- src/index.js
+++ src/index.js
@@ -11,64 +11,113 @@ var Ons = require('react-onsenui');
 
 import CourseDetailPage from './components/CourseDetailPage';
 import CoursesPage from './components/CoursesPage';
+import LoginPage from './components/LoginPage';
+import SignUpPage from './components/SignUpPage';
 
 
 
 import {extendObservable, action, autorun} from 'mobx';
 
 class ListManager {
-  constructor(appState, navigator) {
-    extendObservable(this, {
-      data: [{title: "Corso di programmazione", id: "poppe"}, {title: "Corso di programmazione", id: "poppe"}, {title: "Corso di programmazione", id: "poppe"}, {title: "Corso di programmazione", id: "poppe"}, {title: "Corso di programmazione", id: "poppe"}],
+    constructor(appState, navigator) {
+        extendObservable(this, {
+            data: [{title: "Corso di programmazione", id: "poppe"}, {title: "Corso di programmazione", id: "poppe"}, {title: "Corso di programmazione", id: "poppe"}, {title: "Corso di programmazione", id: "poppe"}, {title: "Corso di programmazione", id: "poppe"}],
 
-      select: action(function(index) {
+            select: action(function(index) {
+                navigator.goToCoursePage(index, this.data[index].id, this.data[index].title)
+            }),
 
-        appState.pages.push({name: 'course', index: index, id: this.data[index].id});
+            get dataNames(){
+                var objs = this.data.map(item => item.title);
+                return(objs)
+            }
+        })
+    }
+}
 
-        navigator.pushPage({title: this.data[index].title, hasBackButton: true})
-      }),
+class AppNavigator{
+    constructor(appState, navigator){
+        this.appState = appState;
+        this.navigator = navigator;
+    }
 
-      get dataNames(){
-        var objs = this.data.map(item => item.title);
-        return(objs)
-      }
-    })
-  }
+    goToCoursePage(index, id, title){
+        console.log("course")
+        this.appState.pages.push({name: 'course', index: index, id: id});
+        this.navigator.pushPage({title: title, hasBackButton: true})
+
+
+    }
+
+    goToCoursesPage(){
+        this.appState.pages.push({name: 'courses'});
+        this.navigator.pushPage({title: 'courses', hasBackButton: false})
+    }
+
+    goBack(){
+        console.log("goback")
+        this.appState.goBack();
+        this.navigator.popPage();
+    }
+
+    login(id){
+        this.goToCoursesPage();
+    }
+
+    signUp(){
+        this.goToCoursesPage();
+    }
+
+    beginSignUp(){
+        this.appState.pages.push({name: 'signUp'});
+        this.navigator.pushPage({title: 'signUp', hasBackButton: true})
+    }
 }
 
 class AppState {
-  constructor(){
-    extendObservable(this, {
-      pages: [{name: 'main'}],
+    constructor(){
+        extendObservable(this, {
+            pages: [{name: 'main'}],
 
-      get lastPage(){
-        return(this.pages.slice(-1)[0])
-      },
+            get lastPage(){
+                return(this.pages.slice(-1)[0])
+            },
 
-      goBack: action(function(){
-        this.pages.pop();
-      }),
+            goBack: action(function(){
+                this.pages.pop();
+            }),
 
-      pageToDisplay: function(route, navigator)  {
+
+        })
+    }
+    pageToDisplay(route, navigator) {
+        var nav = new AppNavigator(this, navigator)
         if(this.lastPage.name === 'main'){
-          var manager = new ListManager(this, navigator);
-          return(<CoursesPage key={route.title} route={route} navigator={navigator} manager={manager}/>)
+            return(<LoginPage key={route.title} navigator={nav}></LoginPage>)
+        }
+        else if(this.lastPage.name === 'signUp'){
+            return(<SignUpPage key={route.title} navigator={nav}></SignUpPage>)
+
+        }      
+        else if(this.lastPage.name === 'courses'){
+            var manager = new ListManager(this, nav);
+            return(<CoursesPage key={route.title} route={route} navigator={nav} manager={manager}/>)
         }
         else if(this.lastPage.name === 'course'){
-          return(<CourseDetailPage key={route.title} route={route} navigator={navigator} onBack={this.goBack.bind(this)}></CourseDetailPage>);
+            return(<CourseDetailPage key={route.title} route={route} navigator={nav} onBack={this.goBack.bind(this)}></CourseDetailPage>);
         }
-      }
-    })
-  }
+    }
+
+
 }
 
 
 var state = new AppState()
 
 ons.ready(function() {
-  ReactDOM.render(
-    <AppContainer stateManager={state}/>,
-    document.getElementById('root')
-  );
+    ReactDOM.render(
+        <AppContainer stateManager={state}/>,
+        document.getElementById('root')
+    );
 });
 
